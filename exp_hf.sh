4134070871b8753a08e3d304ad263d9b935cfb91
lorelupo <lorenzo.lupo93@gmail.com>
2023-10-10 11:01:28 +0200

    improved gender prompt, added gender_tweets prompt

--- exp_hf.sh
+++ exp_hf.sh
@@ -7,16 +7,24 @@
 model_name=google/flan-t5-xxl
 max_len_model=512
 
-for instruction in  gender_classification/bio_hf.txt
-do
-python main.py \
+# CUDA_VISIBLE_DEVICES=0 python main.py \
+#     --data_file data/user_classification/data_for_models_test.pkl \
+#     --instruction instructions/gender_classification/bio_hf.txt \
+#     --task_file tasks/gender_classification/bio.json \
+#     --prompt_suffix \\n\"\"\"\\nGender: \
+#     --model_name $model_name \
+#     --max_len_model $max_len_model \
+#     --output_dir tmp \
+#     --cache_dir /scratch/mentalism/cache/ \
+#     --evaluation_only False
+
+CUDA_VISIBLE_DEVICES=0 python main.py \
     --data_file data/user_classification/data_for_models_test.pkl \
-    --instruction instructions/$instruction \
-    --task_file tasks/gender_classification/bio.json \
-    --prompt_suffix "\\n\"\"\"\\nGender:" \
+    --instruction instructions/gender_classification/bio_tweets_hf.txt \
+    --task_file tasks/gender_classification/bio_tweets.json \
+    --prompt_suffix \\n\"\"\"\\nGender: \
     --model_name $model_name \
     --max_len_model $max_len_model \
     --output_dir tmp \
     --cache_dir /scratch/mentalism/cache/ \
     --evaluation_only False
-done
