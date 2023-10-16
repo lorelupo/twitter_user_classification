05909ee30ce2924e4054c1938dd6e519fb43b809
pb <p.bose@zoho.com>
2023-10-16 19:28:08 +0200

    added robust label coding for number ranges

--- exp_hf_nl.sh
+++ exp_hf_nl.sh
@@ -19,22 +19,23 @@ max_len_model=512
 #     --evaluation_only False
 
 # CUDA_VISIBLE_DEVICES=0 python main.py \
-#     --data_file data/user_classification/data_for_models_german_data.pkl \
+#     --data_file data/user_classification/data_for_models_dutch_data.pkl \
 #     --instruction instructions/gender_classification/bio_tweets_hf.txt \
 #     --task_file tasks/gender_classification/bio_tweets.json \
 #     --prompt_suffix \\n\"\"\"\\nGender: \
 #     --model_name $model_name \
 #     --max_len_model $max_len_model \
-#     --output_dir tmp_de \
+#     --output_dir tmp_nl \
 #     --cache_dir /scratch/mentalism/cache/ \
 #     --evaluation_only False
 
+
 CUDA_VISIBLE_DEVICES=1 python main.py \
-    --data_file data/user_classification/data_for_models_german_data.pkl \
+    --data_file data/user_classification/data_for_models_dutch_data.pkl \
     --instruction instructions/age_classification/bio_tweets_hf.txt \
     --task_file tasks/age_classification/bio_tweets.json \
     --prompt_suffix \\n\"\"\"\\nAge\ group: \
     --model_name $model_name \
     --max_len_model $max_len_model \
-    --output_dir tmp_de \
+    --output_dir tmp_nl\
     --cache_dir /scratch/mentalism/cache/
